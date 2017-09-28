1a73479f3600b52e88cbe1ac4f078d126d7c06e6
chrismakesthething <anim4te@gmail.com>
2017-09-28 13:47:03 +1300

    localstorage handler for username/password toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RouterModule } from '@angular/router';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { LocalStorageModule } from 'angular-local-storage'
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
